cbb933a4bc981a7e1d36c984f990297a9bac12f5
polainait <polaina.mezentseva@mail.ru>
2024-03-24 22:01:10 +0300

    Created generateReport(msisdn) method in ReportGenerator class

--- src/main/java/ru/polainam/ReportGenerator.java
+++ src/main/java/ru/polainam/ReportGenerator.java
@@ -18,12 +18,24 @@ import static ru.polainam.utils.Parser.parseDuration;
 public class ReportGenerator {
     private final ObjectMapper objectMapper;
     private static final int CHARGING_PERIOD = 12;
+    private static final int TABLE_WIDTH = 37;
+
     @Autowired
     public ReportGenerator(ObjectMapper objectMapper) {
         this.objectMapper = objectMapper;
     }
 
     public void generateReport() {
+        Map<String, String> totalCallTimes = getTotalCallTimes(null);
+        printReport(null, totalCallTimes);
+    }
+
+    public void generateReport(String msisdn) {
+        Map<String, String> totalCallTimes = getTotalCallTimes(msisdn);
+        printReport(msisdn, totalCallTimes);
+    }
+
+    private Map<String, String> getTotalCallTimes(String msisdn) {
         Map<String, String> totalCallTimes = new HashMap<>();
         for (int month = 1; month <= CHARGING_PERIOD; month++) {
             File monthDirectory = new File("reports/" + month);
@@ -33,19 +45,27 @@ public class ReportGenerator {
             for (File file : Objects.requireNonNull(monthDirectory.listFiles())) {
                 try {
                     UDR udr = objectMapper.readValue(file, UDR.class);
-                    String msisdn = udr.getMsisdn();
+                    if (msisdn != null && !udr.getMsisdn().equals(msisdn)) {
+                        continue;
+                    }
                     String incomingCallTime = udr.getIncomingCall().getTotalTime() != null ? udr.getIncomingCall().getTotalTime() : "00:00:00";
                     String outcomingCallTime = udr.getOutcomingCall().getTotalTime() != null ? udr.getOutcomingCall().getTotalTime() : "00:00:00";
                     String totalTime = sumCallTimes(incomingCallTime, outcomingCallTime);
                     String totalNewTime = sumCallTimes(totalCallTimes.getOrDefault(msisdn, "00:00:00"), totalTime);
-                    totalCallTimes.put(msisdn, totalNewTime);
+                    totalCallTimes.put(String.valueOf(month), totalNewTime);
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
         }
-        int tableWidth = 37;
-        String dashLine = "-".repeat(tableWidth);
+        return totalCallTimes;
+    }
+
+    private void printReport(String msisdn, Map<String, String> totalCallTimes) {
+        String dashLine = "-".repeat(TABLE_WIDTH);
+        if (msisdn != null) {
+            System.out.println("Report for MSISDN: " + msisdn);
+        }
         System.out.println(dashLine);
         System.out.printf("| %-15s | %-15s |\n", "Number", "Total Call Time");
         System.out.println(dashLine);
